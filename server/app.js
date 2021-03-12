13502774e2504512ffb93be11c27915a6ab90659
unknown <denniswillie2000@gmail.com>
2021-03-12 11:45:14 +0000

    add cors

--- server/app.js
+++ server/app.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const CLIENT_URL = "http://localhost:3000";
+const cors = require("cors");
 const app = express();
-const port = 3000;
+const port = 5000;
+
+app.use(
+    cors({
+      origin: CLIENT_URL,
+      methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+      credentials: true
+    })
+);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
