1735824bfcd117487fe952af2840d320473b0824
Denniswillie <46088356+Denniswillie@users.noreply.github.com>
2021-04-23 13:40:13 +0100

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,13 +1,12 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const CLIENT_URL = "http://localhost:3000";
 const cors = require("cors");
 const app = express();
 const port = 5000;
 
-app.use(bodyParser.json());
+app.use(express.json());
 
-app.use(bodyParser.urlencoded({
+app.use(express.urlencoded({
   extended: true
 }));
 
